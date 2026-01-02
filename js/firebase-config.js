dbe4773867ab7272f5c2fbccc9dc8272a7a6fa2c
seinlain12 <seinlain12@gmail.com>
2026-01-02 18:13:44 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,14 +1,16 @@
-// Firebase 콘솔 -> 프로젝트 설정에서 복사한 본인의 정보를 넣으세요
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
+    apiKey: "AIzaSyDFsKzHtjw9Hc-tPQGs4gnDpqq6VzmpZ3I",
     authDomain: "tjdgns-2e002.firebaseapp.com",
     databaseURL: "https://tjdgns-2e002-default-rtdb.firebaseio.com",
     projectId: "tjdgns-2e002",
-    storageBucket: "tjdgns-2e002.appspot.com",
-    messagingSenderId: "YOUR_ID",
-    appId: "YOUR_APP_ID"
+    storageBucket: "tjdgns-2e002.firebasestorage.app",
+    messagingSenderId: "406888986104",
+    appId: "1:406888986104:web:a1d65601324971c3c0d20d",
+    measurementId: "G-DB42KPYH0B"
 };
 
-// 초기화
-firebase.initializeApp(firebaseConfig);
+// Firebase 초기화
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 const db = firebase.database();
