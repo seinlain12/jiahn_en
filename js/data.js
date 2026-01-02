8cacebf39dd0a8566c67055e94568789e7113a90
seinlain12 <seinlain12@gmail.com>
2026-01-02 20:22:52 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,14 +1,32 @@
-const savedData = localStorage.getItem('studyHubData');
-
-let studyData = savedData ? JSON.parse(savedData) : {
-    logs: {
-        "260101": {
-            chats: [],
-            sentences: []
-        }
-    }
+const firebaseConfig = {
+    apiKey: "AIzaSyDFsKzHtjw9Hc-tPQGs4gnDpqq6VzmpZ3I",
+    authDomain: "tjdgns-2e002.firebaseapp.com",
+    databaseURL: "https://tjdgns-2e002-default-rtdb.firebaseio.com",
+    projectId: "tjdgns-2e002",
+    storageBucket: "tjdgns-2e002.firebasestorage.app",
+    messagingSenderId: "406888986104",
+    appId: "1:406888986104:web:a1d65601324971c3c0d20d",
+    measurementId: "G-DB42KPYH0B"
 };
 
+// Firebase 초기화
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+let studyData = { logs: {} };
+
+// 데이터 로드
+function loadData(callback) {
+    db.ref('studyHubData').on('value', (snapshot) => {
+        const data = snapshot.val();
+        studyData = data || { logs: {} };
+        if (callback) callback();
+    });
+}
+
+// 데이터 저장 (서버 덮어쓰기)
 function saveToStorage() {
-    localStorage.setItem('studyHubData', JSON.stringify(studyData));
+    return db.ref('studyHubData').set(studyData)
+        .then(() => console.log("Cloud Saved"))
+        .catch(err => console.error("Save Error:", err));
 }
